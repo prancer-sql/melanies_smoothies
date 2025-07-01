2cbe5b97fb3a1e62aa04e7e6469258bac7e1d2e9
prancer-sql <davidcorris@btinternet.com>
2025-07-01 16:21:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #get_active_session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 #Convert the snowpark dataframe to a Pandas Dataframe so we can use the LOC function
